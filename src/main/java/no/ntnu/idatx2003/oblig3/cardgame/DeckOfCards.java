b3bfeb6db658d514c33cb7f5fa5e6043f37267e6
OSTERITE <adrian@bandidos.org>
2024-03-04 12:08:08 +0100

    added DeckOfCards class and implemented methods for dealing hand

---
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/DeckOfCards.java
@@ -0,0 +1,51 @@
+package no.ntnu.idatx2003.oblig3.cardgame;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class DeckOfCards {
+
+    private final char[] suits = { 'S', 'H', 'D', 'C' };
+    private final int[] faces = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 };
+
+    private final ArrayList<PlayingCard> deck = new ArrayList<PlayingCard>();
+
+    public DeckOfCards() {
+        this.createDeck();
+        this.shuffle();
+    }
+
+    public void resetCards(){
+        this.deck.clear();
+        this.createDeck();
+        this.shuffle();
+    }
+
+    public Collection<PlayingCard> dealHand(int numberOfCards){
+
+        ArrayList<PlayingCard> hand = new ArrayList<PlayingCard>();
+        for (int i = 0; i < numberOfCards; i++) {
+            // Removes card from deck. This is good if multiple players want to play against eachother.
+            hand.add(deck.remove(0));
+        }
+
+        return hand;
+    }
+
+    private void shuffle(){
+        Collections.shuffle(deck);
+    }
+
+    private void createDeck() {
+        for (char suit : suits) {
+            for (int face : faces) {
+                this.addCard(new PlayingCard(suit, face));
+            }
+        }
+    }
+
+    private void addCard(PlayingCard card) {
+        this.deck.add(card);
+    }
+}
